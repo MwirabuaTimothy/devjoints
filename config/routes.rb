5551730e9d7c7a6b2b99593c19972189a58b38c2
TechyTimo <techytimo@gmail.com>
2013-10-17 12:36:46 +0300

    generated voters and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Devjoints::Application.routes.draw do
+  get "welcome/index"
+  get "welcome/about"
+  resources :voters
+  root "voters#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
